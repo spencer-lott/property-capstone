885f73fe87163b50a21e0a09a1922aa307af30bc
Spencer Lott <spklott@gmail.com>
2023-08-15 11:32:14 -0400

    saving so I can push the employee landing page to github

---
+++ PM Capstone 2/client/pmcapstone/src/homePage/PieChart.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Pie } from "react-chartjs-2";
+
+export const PieChart = ({ chartData }) => {
+    return (
+        <div className="chart-container">
+          <h2 className="h3-property-details" style={{ textAlign: "center" }}>Vacancies</h2>
+          <Pie
+            data={chartData}
+            options={{
+              plugins: {
+                title: {
+                  display: true,
+                  text: "Current status of vacancies"
+                }
+              }
+            }}
+          />
+        </div>
+      );
+}
