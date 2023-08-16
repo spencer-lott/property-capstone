54c41a302cf67d5c782816c3c0301db6440386b8
Spencer Lott <spklott@gmail.com>
2023-08-16 08:51:42 -0400

    cleaned up code and added more seed data

--- PM Capstone 2/client/pmcapstone/src/Auth/Login.js
+++ PM Capstone 2/client/pmcapstone/src/Auth/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { login } from "../APIManagers/UserProfileManager";
 
 export default function Login({setIsLoggedIn}) {
--- PM Capstone 2/client/pmcapstone/src/maintenanceHistory/MaintenanceHistory.js
+++ PM Capstone 2/client/pmcapstone/src/maintenanceHistory/MaintenanceHistory.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Link, useNavigate, useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { getAllUserProfiles } from "../APIManagers/UserProfileManager"
 import { deleteMaintenanceHistory, getMaintenanceHistoryByPropertyId } from "../APIManagers/MaintenanceHistoryManager"
 
